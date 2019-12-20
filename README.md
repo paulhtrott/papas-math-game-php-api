ed77c5fdaed1af71708486ec4709fcb8e65e7d44
Paul Trott <paulhtrott@gmail.com>
2019-12-20 00:06:29 -0600

    Started on php for api

---
+++ README.md
@@ -0,0 +1,7 @@
+# PHP Application for Papas Math Game Api.
+
+Run local server
+
+```sh
+php -S localhost:8080
+```
---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Papas Math Game</title>
+  </head>
+  <body>
+    <h1>Papa's Math Game</h1>
+    <p>Find our application on the Apple Store.</p>
+    <p><strong>Run local server:</strong> php -S localhost:8080</p>
+  </body>
+</html>
